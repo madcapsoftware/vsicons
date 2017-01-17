fd52e5e619a6b1bda3850387320908c7ec8a887a
Cherry Wang (Aquent LLC) <v-cherrw@microsoft.com>
2017-01-17 12:31:58 -0800

    removed limit to show full list, but haven't added lazye load yet

--- src/_js/main.js
+++ src/_js/main.js
@@ -9,4 +9,4 @@ ReactDOM.render(<VsilHeader />, document.getElementById('VsilHeader'));
 
 ReactDOM.render(<VsilSearch target="VsilGallery" />, document.getElementById('VsilSearch'));
 
-ReactDOM.render(<VsilGallery dataurl="vsicons.json" prefix="https://vsicons.blob.core.windows.net/assets/DevEnv/" suffix="_16x.svg" limit={100} />, document.getElementById('VsilGallery'));
+ReactDOM.render(<VsilGallery dataurl="vsicons.json" prefix="https://vsicons.blob.core.windows.net/assets/DevEnv/" suffix="_16x.svg" />, document.getElementById('VsilGallery'));
