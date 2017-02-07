408cfaf72baa91f662c8cc7cb43077a58f620cac
Cherry Wang (Aquent LLC) <v-cherrw@microsoft.com>
2017-02-07 10:47:28 -0800

    Used List component instead of plain table for better render performance

--- src/_js/components/VsilGallery.js
+++ src/_js/components/VsilGallery.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Image } from 'office-ui-fabric-react/lib/index';
+import { Image, List } from 'office-ui-fabric-react/lib/index';
 import * as axios from 'axios';
 
 export default class VsilGallery extends React.Component {
@@ -45,29 +45,28 @@ export default class VsilGallery extends React.Component {
   }
 
   render() {
+    const items = this.state.items;
     return (
-      <table className="vsil-gallery">
-        <thead className="ms-Grid">
-          <tr className="ms-bgColor-neutralSecondary ms-fontColor-white ms-Grid-row">
-            <th className="ms-Grid-col ms-u-sm9 ms-u-md4"><span>Name</span></th>
-            <th className="ms-Grid-col ms-u-sm3 ms-u-md2"><span>Icon</span></th>
-            <th className="ms-Grid-col ms-u-hiddenMdDown ms-u-md6"><span>Description</span></th>
-          </tr>
-        </thead>
-        <tbody className="ms-Grid">
-          {
-            this.state.items.map((item) => {
-              return (
-                <tr className="ms-Grid-row vsil-gallery-item" id={`Item_${item.id}`}>
-                  <td className="ms-Grid-col ms-u-sm9 ms-u-md4 vsil-gallery-item-name"><span>{item.name}</span></td>
-                  <td className="ms-Grid-col ms-u-sm3 ms-u-md2 vsil-gallery-thumbnail"><span><Image src={`${this.props.prefix}${item.name}/${item.name}${this.props.suffix}`} alt={` ${item.name} `} width={32} /></span></td>
-                  <td className="ms-Grid-col ms-u-hiddenMdDown ms-u-md6 vsil-gallery-item-description"><span>{item.description || ''}</span></td>
-                </tr>
-              );
-            })
-          }
-        </tbody>
-      </table>
+      <div className="vsil-gallery ms-Grid">
+          <div className="ms-bgColor-neutralSecondary ms-fontColor-white ms-Grid-row vsil-gallery-header">
+            <div className="ms-Grid-col ms-u-sm9 ms-u-md4"><span>Name</span></div>
+            <div className="ms-Grid-col ms-u-sm3 ms-u-md2"><span>Icon</span></div>
+            <div className="ms-Grid-col ms-u-hiddenMdDown ms-u-md6"><span>Description</span></div>
+          </div>
+        <div className="vsil-gallery-body">
+        <List items={ items } onRenderCell={ (item, index) => (
+          <div className="ms-Grid-row vsil-gallery-item" id={`Item_${item.id}`} data-keywords={ item.keywords }>
+                      <div className="ms-Grid-col ms-u-sm9 ms-u-md4 vsil-gallery-item-name"><span>{item.name}</span></div>
+                      <div className="ms-Grid-col ms-u-sm3 ms-u-md2 vsil-gallery-thumbnail"><span><Image src={`https://vsicons.blob.core.windows.net/assets/DevEnv/${item.name}/${item.name}_16x.svg`} alt={` ${item.name} `} width={32} /></span></div>
+                      <div className="ms-Grid-col ms-u-hiddenMdDown ms-u-md6 vsil-gallery-item-description"><span>{item.description || ''}</span></div>
+          </div>
+        ) }
+        />
+        </div>
+        <div className="vsil-gallery-footer">
+          <p className="ms-fontColor-neutralSecondary ms-u-textAlignRight">Displaying { items.length } Visual Studio icons.</p>
+        </div>
+      </div>
     );
   }
 }
