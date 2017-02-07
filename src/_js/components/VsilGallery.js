d7600e60185c6d2847cbaf210908673f253a8bb8
Cherry Wang (Aquent LLC) <v-cherrw@microsoft.com>
2017-02-07 13:00:10 -0800

    fixed json array; removed padding around list

--- src/_js/components/VsilGallery.js
+++ src/_js/components/VsilGallery.js
@@ -47,22 +47,20 @@ export default class VsilGallery extends React.Component {
   render() {
     const items = this.state.items;
     return (
-      <div className="vsil-gallery ms-Grid">
+      <div className="vsil-gallery">
           <div className="ms-bgColor-neutralSecondary ms-fontColor-white ms-Grid-row vsil-gallery-header">
             <div className="ms-Grid-col ms-u-sm9 ms-u-md4"><span>Name</span></div>
             <div className="ms-Grid-col ms-u-sm3 ms-u-md2"><span>Icon</span></div>
             <div className="ms-Grid-col ms-u-hiddenMdDown ms-u-md6"><span>Description</span></div>
           </div>
-        <div className="vsil-gallery-body">
-        <List items={ items } onRenderCell={ (item, index) => (
-          <div className="ms-Grid-row vsil-gallery-item" id={`Item_${item.id}`} data-keywords={ item.keywords }>
-                      <div className="ms-Grid-col ms-u-sm9 ms-u-md4 vsil-gallery-item-name"><span>{item.name}</span></div>
-                      <div className="ms-Grid-col ms-u-sm3 ms-u-md2 vsil-gallery-thumbnail"><span><Image src={`https://vsicons.blob.core.windows.net/assets/DevEnv/${item.name}/${item.name}_16x.svg`} alt={` ${item.name} `} width={32} /></span></div>
-                      <div className="ms-Grid-col ms-u-hiddenMdDown ms-u-md6 vsil-gallery-item-description"><span>{item.description || ''}</span></div>
-          </div>
-        ) }
-        />
-        </div>
+          <List className="vsil-gallery-body" items={ items } onRenderCell={ (item, index) => (
+            <div className="ms-Grid-row vsil-gallery-item" id={`Item_${item.id}`} data-keywords={ item.keywords }>
+              <div className="ms-Grid-col ms-u-sm9 ms-u-md4 vsil-gallery-item-name"><span>{item.name}</span></div>
+              <div className="ms-Grid-col ms-u-sm3 ms-u-md2 vsil-gallery-thumbnail"><span><Image src={`https://vsicons.blob.core.windows.net/assets/DevEnv/${item.name}/${item.name}_16x.svg`} alt={` ${item.name} `} width={32} /></span></div>
+              <div className="ms-Grid-col ms-u-hiddenMdDown ms-u-md6 vsil-gallery-item-description"><span>{item.description || ''}</span></div>
+            </div>
+          ) }
+          />
         <div className="vsil-gallery-footer">
           <p className="ms-fontColor-neutralSecondary ms-u-textAlignRight">Displaying { items.length } Visual Studio icons.</p>
         </div>
