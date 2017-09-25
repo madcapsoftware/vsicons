f6d6005d0fd0c0731c1df4fb8122e5568ce90211
Cherry Wang <chryw@catenology.com>
2017-09-25 11:27:49 -0700

    Added new icons; added upload task

--- gulpfile.js/upload.js
+++ gulpfile.js/upload.js
@@ -1,26 +1,27 @@
 const azure = require('azure-storage');
 const foreach = require('gulp-foreach');
+const path = require('path');
 const gulp = require('gulp');
 const fs = require('fs');
 require('dotenv').config();
 
 const blobService = azure.createBlobService(process.env.AZURESTORAGE_CONNECTIONSTRING);
-const sourceFolder = 'checkin';
+const sourceFolder = 'storage';
 
 fs.openSync('upload.log', 'w');
 
 gulp.task('upload', () => {
-  return gulp.src(`${sourceFolder}**\\*.*`)
-  .pipe(foreach((stream, file) => {
-    const localPath = file.path;
-    const remotePath = `DevEnv\\${localPath.replace(sourceFolder, '')}`;
-    blobService.createBlockBlobFromLocalFile('assets', remotePath, localPath, (error, result) => {
-      if (!error) {
-        fs.appendFileSync('upload.log', `${result.name} uploaded to ${result.container}\n`);
-      } else {
-        fs.appendFileSync('upload.log', `Failed to upload ${localPath}. ${error}`);
-      }
-    });
-    return stream;
-  }));
+  gulp.src(`${sourceFolder}\\**\\*.*`)
+    .pipe(foreach((stream, file) => {
+      const filePath = file.path;
+      const fileName = path.basename(filePath);
+      const folderName = fileName.replace(/_.+/g, '');
+      const blobName = `DevEnv/${folderName}/${fileName}`;
+      blobService.createBlockBlobFromLocalFile('assets', blobName, filePath, (error, result) => {
+        if (!error) {
+          console.log(`${result.name} uploaded to ${result.container}/${blobName}`);
+        }
+      });
+      return stream;
+    }));
 });
