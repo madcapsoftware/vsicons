6dd6bdd74a8f04cede40d0988b4bcf443824de8d
Cherry Wang <chryw@catenology.com>
2018-02-16 18:19:41 -0800

    Added new icon metadata; updated upload.js to only upload new files

--- gulpfile.js/upload.js
+++ gulpfile.js/upload.js
@@ -1,27 +1,58 @@
 const azure = require('azure-storage');
-const foreach = require('gulp-foreach');
-const path = require('path');
 const gulp = require('gulp');
+const glob = require('glob');
 const fs = require('fs');
+const hasha = require('hasha');
 require('dotenv').config();
 
 const blobService = azure.createBlobService(process.env.AZURESTORAGE_CONNECTIONSTRING);
-const sourceFolder = 'storage';
-
-fs.openSync('upload.log', 'w');
+const srcFolder = 'E:/Desktop/temp/test/';
+const globFilter = '!(*color|*grey|*inverse|*cyan|*disable|*noHalo|*nohalo)*.{svg,png,xaml}';
+const destFolder = '.';
+const container = 'vsideicons';
+const logStream = fs.createWriteStream('upload.log', { flags: 'a' });
 
 gulp.task('upload', () => {
-  gulp.src(`${sourceFolder}\\**\\*.*`)
-    .pipe(foreach((stream, file) => {
-      const filePath = file.path;
-      const fileName = path.basename(filePath);
-      const folderName = fileName.replace(/_.+/g, '');
-      const blobName = `DevEnv/${folderName}/${fileName}`;
-      blobService.createBlockBlobFromLocalFile('assets', blobName, filePath, (error, result) => {
-        if (!error) {
-          console.log(`${result.name} uploaded to ${result.container}/${blobName}`);
+  const localBlobs = glob.sync(`${srcFolder}/**/${globFilter}`);
+  logStream.write(`\r\n\n-------${new Date()}------\r\n\n`);
+  localBlobs.forEach((localBlob) => {
+    const fileName = localBlob.replace(srcFolder, '');
+    const localMD5 = hasha.fromFileSync(localBlob, { algorithm: 'md5', encoding: 'base64' });
+    const localMtime = fs.statSync(localBlob).mtime;
+    blobService.doesBlobExist(container, fileName, (existsError, existsResult) => {
+      if (!existsError) {
+        if (existsResult.exists) {
+          const remoteMtime = new Date(existsResult.lastModified);
+          const remoteMD5 = existsResult.contentSettings.contentMD5;
+          if (remoteMD5 === localMD5 || remoteMtime > localMtime) {
+            logStream.write(`Skipping ${fileName} as same or newer version already exists in ${container}.\r\n`);
+          } else {
+            blobService.createBlockBlobFromLocalFile(
+              container,
+              `${destFolder}/${fileName}`,
+              localBlob,
+              (createError) => {
+                if (!createError) {
+                  logStream.write(`${fileName} was replaced by newer version.\r\n`);
+                } else {
+                  logStream.write(`${createError}\r\n`);
+                }
+              });
+          }
+        } else {
+          blobService.createBlockBlobFromLocalFile(
+            container,
+            `${destFolder}/${fileName}`,
+            localBlob,
+            (createError) => {
+              if (!createError) {
+                logStream.write(`Successfully uploaded ${fileName} to ${container}/${destFolder}\r\n`);
+              } else {
+                logStream.write(`${createError}\r\n`);
+              }
+            });
         }
-      });
-      return stream;
-    }));
+      }
+    });
+  });
 });
