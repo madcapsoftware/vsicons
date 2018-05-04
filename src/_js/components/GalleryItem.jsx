b87a01f6b1eb45842b6c75caaa062bf69b71ae36
Cherry Wang <chryw@catenology.com>
2018-05-04 12:22:46 -0700

    minor tweaks to galeryitem

--- src/_js/components/GalleryItem.jsx
+++ src/_js/components/GalleryItem.jsx
@@ -2,13 +2,15 @@ import * as React from 'react';
 import { Image } from 'office-ui-fabric-react/lib/index';
 import DownloadButton from './DownloadButton.jsx';
 
+const urlPrefix = 'https://vsicons.blob.core.windows.net/vsideicons';
+
 const GalleryItem = props => (
   <div className="ms-Grid-row vsil-gallery-item" id={`Item_${props.id}`}>
     <div className="ms-Grid-col ms-u-sm8 ms-u-md4 vsil-gallery-item-name">
       <span>{props.name}</span>
     </div>
     <div className="ms-Grid-col ms-u-sm4 ms-u-md2 vsil-gallery-thumbnail">
-      <span><Image src={`https://vsicons.blob.core.windows.net/assets/DevEnv/${props.name}/${props.name}_16x.svg`} alt={` ${props.name} `} width={32} /></span>
+      <span><Image src={`${urlPrefix}/${props.name}/${props.name}_16x.svg`} alt={` ${props.name} `} width={32} /></span>
     </div>
     <div
       className="ms-Grid-col ms-u-hiddenMdDown ms-u-lg4 vsil-gallery-item-description"
@@ -27,22 +29,22 @@ const GalleryItem = props => (
               key: 'svg',
               name: 'Responsive SVG',
               title: 'Responsive SVG',
-              href: `https://vsicons.blob.core.windows.net/assets/DevEnv/${props.name}/${props.name}_16x.svg`,
-              download: true,
+              href: `${urlPrefix}/${props.name}/${props.name}_16x.svg`,
+              download: `${urlPrefix}/${props.name}/${props.name}_16x.svg`,
             },
             {
               key: 'png',
               name: '16x16 PNG',
               title: '16x16 PNG',
-              href: `https://vsicons.blob.core.windows.net/assets/DevEnv/${props.name}/${props.name}_16x.png`,
-              download: true,
+              href: `${urlPrefix}/${props.name}/${props.name}_16x.png`,
+              download: `${urlPrefix}/${props.name}/${props.name}_16x.png`,
             },
             {
               key: 'xaml',
               name: 'XAML',
               title: 'XAML for ImageService',
-              href: `https://vsicons.blob.core.windows.net/assets/DevEnv/${props.name}/${props.name}_16x.xaml`,
-              download: true,
+              href: `${urlPrefix}/${props.name}/${props.name}_16x.xaml`,
+              download: `${urlPrefix}/${props.name}/${props.name}_16x.xaml`,
             },
           ]}
         />
