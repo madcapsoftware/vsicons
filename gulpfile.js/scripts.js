ed05513ac6f6cd507d4df028cd47004947133294
Rick Martin <rickpmartin@gmail.com>
2018-11-01 15:53:54 -0700

    Replace browserify with webpack.

--- gulpfile.js/scripts.js
+++ gulpfile.js/scripts.js
@@ -1,18 +1,10 @@
 const gulp = require('gulp');
-const browserify = require('browserify');
-const rename = require('gulp-rename');
-const source = require('vinyl-source-stream');
+const webpack = require('webpack');
+const webpackStream = require('webpack-stream');
+const webpackConfig = require('../webpack.config.js');
 
 gulp.task('scripts', ['copy'], () => (
-  browserify('src/_js/main.jsx')
-      .transform('babelify', {
-        presets: ['env', 'react'],
-      })
-      .bundle()
-      .pipe(source('main.jsx'))
-      .pipe(rename({
-        basename: 'main.min',
-        extname: '.js',
-      }))
+  gulp.src('src/_js/main.jsx')
+      .pipe(webpackStream(webpackConfig), webpack)
       .pipe(gulp.dest('dist/js'))
 ));
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  output: {
+    filename: 'main.min.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+}
